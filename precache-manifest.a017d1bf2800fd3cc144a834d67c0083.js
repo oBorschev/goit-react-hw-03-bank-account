248531767bc36f3c0890a1eb7981fa4924c7a438
Oleg <bosoleg89@gmail.com>
2019-12-10 22:21:55 +0200

    Updates

---
+++ precache-manifest.a017d1bf2800fd3cc144a834d67c0083.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e4a6347ce264592c7c74db22e0385aa",
+    "url": "/oBorschev/goit-react-hw-03-bank-account/index.html"
+  },
+  {
+    "revision": "8949c26369c3c7fad889",
+    "url": "/oBorschev/goit-react-hw-03-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "f735128859906935b074",
+    "url": "/oBorschev/goit-react-hw-03-bank-account/static/css/main.3f8bb1fd.chunk.css"
+  },
+  {
+    "revision": "8949c26369c3c7fad889",
+    "url": "/oBorschev/goit-react-hw-03-bank-account/static/js/2.aee7313e.chunk.js"
+  },
+  {
+    "revision": "f735128859906935b074",
+    "url": "/oBorschev/goit-react-hw-03-bank-account/static/js/main.bace90f9.chunk.js"
+  },
+  {
+    "revision": "776054b6796f85bd6fa1",
+    "url": "/oBorschev/goit-react-hw-03-bank-account/static/js/runtime-main.dcf89776.js"
+  }
+]);
